8dfce4b8be5f45c90a665397a42daf8a0c34030b
Mahmoud5704 <mhnh5704@gmail.com>
2025-11-22 20:49:46 +0200

    update studentquizrecord

--- backend/StudentQuizRecord.java
+++ backend/StudentQuizRecord.java
@@ -1,32 +1,76 @@
 package backend;
+import java.util.Set;
+import java.util.HashSet;
 
-public  class StudentQuizRecord {
+public class StudentQuizRecord {
+    
+    
+    private Lesson lesson;//want id
+    private Quiz quiz;
+    private double score;//to double
+    private boolean passed;//want as it
+    private int passScoreAtAttempt;   
+    private int numberOfTryAtAttempt; 
+   
+    // nprivate Lesson lesson;
 
-    private String lessonId;
-    private Double quizMark;
-    private int remainingTrials;
-    private boolean isLessonCompleted;
-
-    public StudentQuizRecord(String lessonId, Double quizMark, int remainingTrials, boolean isLessonCompleted) {
-        this.lessonId = lessonId;
-        this.quizMark = quizMark;
-        this.remainingTrials = remainingTrials;
-        this.isLessonCompleted = isLessonCompleted;
+    public StudentQuizRecord(Lesson lesson, double score, boolean passed) {
+        
+        this.lesson = lesson;
+        this.quiz = lesson.getQuiz();
+        this.score = score;
+        this.passed = passed;
+        this.passScoreAtAttempt = quiz.getPassScore();
+        this.numberOfTryAtAttempt = quiz.getNumberOfTry();
+       
+    }
+    
+    
+    public StudentQuizRecord(Lesson lesson) {
+        this.lesson = lesson;
+        this.quiz = lesson.getQuiz();
+       
+        this.score = -1;
+        this.passed = false;
+        
+        
+                this.passScoreAtAttempt = quiz.getPassScore();
+        this.numberOfTryAtAttempt = quiz.getNumberOfTry();
+        
+        
     }
 
-    public String getLessonId() {
-        return lessonId;
+    public String getLessonId() {//tmam bezn allah
+        return lesson.getId();
+    }
+    
+    public void complete(int score, boolean passed) {
+        this.score = score;
+        this.passed = passed;
+        
+       
+    }
+    
+
+    public Quiz getQuiz() {
+        return quiz;
     }
 
-    public Double getQuizMark() {
-        return quizMark;
+    public double getScore() {//tmam bezn allah
+        return score;
     }
 
-    public int getRemainingTrials() {
-        return remainingTrials;
+    public boolean isPassed() {//tmam bezn allah
+        return passed;
     }
-
-    public boolean isLessonCompleted() {
-        return isLessonCompleted;
+    
+    public int getPassScoreAtAttempt() {
+        return passScoreAtAttempt; 
     }
+    
+    public int getNumberOfTryAtAttempt() { 
+        return numberOfTryAtAttempt; 
+    }
+    
 }
+
